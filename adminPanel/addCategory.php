605a176cfbc88597f7094d1957e2569f8ad4ab95
AqsaY1234 <kinzaaptech@gmail.com>
2025-04-03 12:45:52 +0500

    add

--- adminPanel/addCategory.php
+++ adminPanel/addCategory.php
@@ -9,28 +9,27 @@ if(!isset($_SESSION['adminEmail'])){
             <!-- Blank Start -->
             <div class="container-fluid pt-4 px-4">
                 <div class="row vh-100 bg-light rounded   mx-0">
-                    <div class="col-md-6 ">
-              
+                    <div class="col-md-6 ">             
                         <div class="bg-light rounded h-100 p-4">
                             <h6 class="mb-4">Add Category Form</h6>
-                            <form>
+                            <form method="post" enctype="multipart/form-data" >
                                 <div class="mb-3">
                                     <label for="exampleInputEmail1" class="form-label">Category Name</label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1"
+                                    <input value="<?php echo $categoryName?>" name="cName" type="text" class="form-control" id="exampleInputEmail1"
                                         aria-describedby="emailHelp">
-                                    <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.
-                                    </div> -->
+                                        <small class="text-danger"><?php echo $categoryNameErr?></small>
                                 </div>
                                 <div class="mb-3">
                                     <label for="exampleInputPassword1" class="form-label">Category Description</label>
-                                   <textarea name="" class="form-control" id=""></textarea>
+                                   <textarea  name="cDes" class="form-control" id=""><?php echo $categoryDes?></textarea>
+                                   <small class="text-danger"><?php echo $categoryDesErr?></small>
                                 </div>
                                 <div class="mb-3">
                                     <label for="exampleInputPassword1" class="form-label">Category Image</label>
-                                  <input type="file" class="form-control">
-                                </div>
-                                
-                                <button type="submit" class="btn btn-primary">Add Category</button>
+                                  <input name="cImage" type="file" class="form-control">
+                                  <small class="text-danger"><?php echo $categoryImageNameErr?></small>
+                                </div>                             
+                                <button name="addCategory" type="submit" class="btn btn-primary">Add Category</button>
                          </form>                   
                     </div>
                     </div>
