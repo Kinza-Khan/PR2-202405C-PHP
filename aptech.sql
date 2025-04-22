61437bac7bc2329e978b1e2151da03ac3fc90daa
talib <talibmari123@gmail.com>
2025-04-22 11:07:47 +0500

    'db'

--- aptech.sql
+++ aptech.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2025 at 10:08 AM
+-- Generation Time: Apr 22, 2025 at 08:07 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,8 +40,9 @@ CREATE TABLE `categories` (
 
 INSERT INTO `categories` (`id`, `name`, `description`, `image`) VALUES
 (1, 'Shoes', 'Khaki Comfy Slip On Sneakers 0120567', 'sh1.webp'),
-(2, 'Bags', 'Color-Block Crossbody Bags for Women Leather Cross Body Purses Cute Designer Handbags Shoulder Bag Medium Size', 'bag.jpg'),
-(3, 'Watches', 'As the official retailer of branded watches, we offer many top brands, including TAG Heuer, Emporio Armani, Guess and many more. These brands are well known fo', 'w1.webp');
+(2, 'Bags', 'Color-Block Crossbody Bags for Women Leather Cross Body Purses Cute Designer Handbags Shoulder Bag Medium Size', 'bag1.webp'),
+(3, 'Watches', 'As the official retailer of branded watches, we offer many top brands, including TAG Heuer, Emporio Armani, Guess and many more. These brands are well known fo', 'w1.webp'),
+(5, 'Perfumes', 'Liquid Gold Perfume For Men And Women\r\nDark and alluring; combining exotic fruits and intoxicating blooms with warm labdanum. The opening is a luminous sweet blend of passion fruit, pineapple, and raspberry, which gets mingled with fresh nuances of jasmine. Floral tones deepen at the heart when cinn', 'p1.webp');
 
 -- --------------------------------------------------------
 
@@ -64,7 +65,11 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `description`, `qty`, `image`, `c_id`) VALUES
-(1, 'Washington | 40MM', 8000, 'Sveston Stylish Washington Is a Gents Stainless Steel Watch having 40mm Dial and comes with luxury watch case', 75, 'w2.webp', 3);
+(1, 'Washington | 40MM', 8000, 'Sveston Stylish Washington Is a Gents Stainless Steel Watch having 40mm Dial and comes with luxury watch case', 75, 'w2.webp', 3),
+(2, 'Casio MDV106 Series ', 8900, 'Black Dial with date window at 3 O\'clock\r\n', 90, 'w1.jpg', 3),
+(3, 'Enigma Perfume For Women', 1200, 'Enigma is a refined fragrance that harmonizes crisp citrus, delicate florals, and soft musk. A luminous blend of freshness and sensuality, it embodies elegance and modern sophistication in every note.', 76, 'p2.webp', 5),
+(4, 'High Life Perfume For Women', 1500, 'Absolutely perfect just the way you are. High Life is a fragrance that will inspire you to live your best life with a spray of sparkling juicy grapefruit, a flood of rose, peony and geranium essences, and a spicy anchor of tonka bean and vanilla.', 100, 'p3.webp', 5),
+(5, 'Silver Mini Bag IWW-MNB24-010', 20000, 'Shine bright with our metallic mini bag, the perfect statement accessory to elevate any outfit. Its eye-catching metallic finish adds a touch of glam and modern flair,', 75, 'bag1.webp', 2);
 
 -- --------------------------------------------------------
 
@@ -112,19 +117,6 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `role_id`) VALUES
 --
 
 --
--- Indexes for table `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `c_id` (`c_id`);
-
---
 -- Indexes for table `role`
 --
 ALTER TABLE `role`
@@ -142,18 +134,6 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
@@ -168,12 +148,6 @@ ALTER TABLE `users`
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `products`
---
-ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `categories` (`id`);
 
 --
 -- Constraints for table `users`
